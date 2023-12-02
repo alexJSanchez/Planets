8918d9eee849ae681ecc38c58b8a89c3c79825ff
alex sanchez <alexjsanchez1020@gmail.com>
2023-12-02 16:52:38 -0500

    add nav

--- planet-facts/src/index.js
+++ planet-facts/src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
-import { BrowserRouter, Link, Links } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Nav from "./components/Nav";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
@@ -7,9 +8,9 @@ import reportWebVitals from "./reportWebVitals";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-	<React.StrictMode>
-		<App />
-	</React.StrictMode>
+	<BrowserRouter>
+		<Nav />
+	</BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
