5562b0ddb8fc5d5d80cd052db256f525b7f43650
alex sanchez <alexjsanchez1020@gmail.com>
2023-12-02 16:14:11 -0500

    add images

--- planet-facts/src/App.js
+++ planet-facts/src/App.js
@@ -1,8 +1,16 @@
 import logo from "./logo.svg";
+import images from "./assets/images";
+import backgroundstars from "./assets/geology-mars.png";
 import "./App.css";
+import "./index.css";
 
 function App() {
-	return <div className=""></div>;
+	return (
+		<div className="bg-backgroundstars w-full h-full">
+			sdfksjdklf
+			<img className="" src={backgroundstars} />
+		</div>
+	);
 }
 
 export default App;
---
+++ planet-facts/src/assets/images.js
@@ -0,0 +1,5 @@
+const images = {
+	"background-stars": "url('/assets/background-stars.svg')",
+};
+
+export default images;
