a5722b13474b012484ebe94c9205f9b15458c778
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-12-04 01:11:14 -0500

    add font

--- planet-facts/tailwind.config.js
+++ planet-facts/tailwind.config.js
@@ -2,21 +2,21 @@
 module.exports = {
   content: ["./src/**/*.{html,js}"],
   theme: {
+    colors: {
+      mysteriousDepth: "#070724",
+      carbonBlue: "#38384F",
+      formalAffair: "#838391",
+      mercury: "#419EBB",
+      venus: "#EDA249",
+      earth: "#6D2ED5",
+      mars: "#D14C32",
+      jupiter: "#D83A34",
+      saturn: "#CD5120",
+      uranus: "#1EC1A2",
+      neptune: "#2D68F0",
+      hokkaidoLavender: "#6f2ed6",
+    },
     extend: {
-      colors: {
-        mysteriousDepth: "#070724",
-        carbonBlue: "#38384F",
-        formalAffair: "#838391",
-        mercury: "#419EBB",
-        venus: "#EDA249",
-        earth: "#6D2ED5",
-        mars: "#D14C32",
-        jupiter: "#D83A34",
-        saturn: "#CD5120",
-        uranus: "#1EC1A2",
-        neptune: "#2D68F0",
-        hokkaidoLavender: "#6f2ed6",
-      },
       backgroundImage: {
         backgroundStars: "url('/src/assets/planet-mars.svg')",
       },
