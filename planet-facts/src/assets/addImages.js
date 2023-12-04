3a5da3f2c61ce46a3e7048ddfb2a0e5ef242f2cc
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-12-04 01:40:44 -0500

    add images

--- planet-facts/src/assets/addImages.js
+++ planet-facts/src/assets/addImages.js
@@ -8,7 +8,25 @@ import geologyNeptune from "./geology-neptune.png";
 import geologySaturn from "./geology-saturn.png";
 import geologyUranus from "./geology-uranus.png";
 import geologyVenus from "./geology-venus.png";
-
+import iconChevron from "./icon-chevron.svg";
+import iconHamburger from "./icon-hamburger.svg";
+import iconSource from "./icon-source.svg";
+import planetEarthInternal from "./planet-earth-internal.svg";
+import planetEarth from "./planet-earth.svg";
+import planetJupiterInternal from "./planet-jupiter-internal.svg";
+import planetJupiter from "./planet-jupiter.svg";
+import planetMarsInternal from "./planet-mars-internal.svg";
+import planetMars from "./planet-mars.svg";
+import planetMercuryInternal from "./planet-mercury-internal.svg";
+import planetMercury from "./planet-mercury.svg";
+import planetNeptuneInternal from "./planet-neptune-internal.svg";
+import planetNeptune from "./planet-neptune.svg";
+import planetSaturnInternal from "./planet-saturn-internal.svg";
+import planetSaturn from "./planet-saturn.svg";
+import planetUranusInternal from "./planet-uranus-internal.svg";
+import planetUranus from "./planet-uranus.svg";
+import planetVenusInternal from "./planet-venus-internal.svg";
+import planetVenus from "./planet-venus.svg";
 export {
   backgroundStars,
   favicon,
@@ -20,4 +38,23 @@ export {
   geologySaturn,
   geologyUranus,
   geologyVenus,
+  iconChevron,
+  iconHamburger,
+  iconSource,
+  planetEarth,
+  planetEarthInternal,
+  planetJupiter,
+  planetJupiterInternal,
+  planetMars,
+  planetMarsInternal,
+  planetMercury,
+  planetMercuryInternal,
+  planetNeptune,
+  planetNeptuneInternal,
+  planetSaturn,
+  planetSaturnInternal,
+  planetUranus,
+  planetUranusInternal,
+  planetVenus,
+  planetVenusInternal,
 };
