baaa152ce587e6637e9c9bf7c73624b1607f36cf
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-12-04 00:58:02 -0500

    add colors

--- planet-facts/tailwind.config.js
+++ planet-facts/tailwind.config.js
@@ -1,12 +1,26 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-	content: ["./src/**/*.{html,js}"],
-	theme: {
-		extend: {
-			backgroundImage: {
-				backgroundstars: "url('/src/assets/planet-mars.svg')",
-			},
-		},
-	},
-	plugins: [],
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        mysteriousDepth: "#070724",
+        carbonBlue: "#38384F",
+        formalAffair: "#838391",
+        mercury: "#419EBB",
+        venus: "#EDA249",
+        earth: "#6D2ED5",
+        mars: "#D14C32",
+        jupiter: "#D83A34",
+        saturn: "#CD5120",
+        uranus: "#1EC1A2",
+        neptune: "#2D68F0",
+        hokkaidoLavender: "#6f2ed6",
+      },
+      backgroundImage: {
+        backgroundStars: "url('/src/assets/planet-mars.svg')",
+      },
+    },
+  },
+  plugins: [],
 };
