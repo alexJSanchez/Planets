2a9fc8c33c8335fe68639c2e68b1475b7496d5ad
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-12-04 01:27:03 -0500

    add images

---
+++ planet-facts/src/assets/addImages.js
@@ -0,0 +1,23 @@
+import backgroundStars from "./background-stars.svg";
+import favicon from "./favicon-32x32.png";
+import geologyEarth from "./geology-earth.png";
+import geologyJupiter from "./geology-jupiter.png";
+import geologyMars from "./geology-mars.png";
+import geologyMercury from "./geology-mercury.png";
+import geologyNeptune from "./geology-neptune.png";
+import geologySaturn from "./geology-saturn.png";
+import geologyUranus from "./geology-uranus.png";
+import geologyVenus from "./geology-venus.png";
+
+export {
+  backgroundStars,
+  favicon,
+  geologyEarth,
+  geologyJupiter,
+  geologyMars,
+  geologyMercury,
+  geologyNeptune,
+  geologySaturn,
+  geologyUranus,
+  geologyVenus,
+};
--- planet-facts/src/components/Nav.js
+++ planet-facts/src/components/Nav.js
@@ -1,9 +1,10 @@
 import "../App.css";
-
+import { geologyEarth } from "../assets/addImages";
 function Nav() {
   return (
     <div className="">
-      <p className="uppercase">The Plantes</p>
+      <p className="uppercase">The Plantes </p>
+      <img src={geologyEarth} alt="geologyEarth" />
     </div>
   );
 }
