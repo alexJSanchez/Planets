25d44de5a0e9e831867f363a240ac145bf5a0c88
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-12-04 03:17:55 -0500

    on click dropdown

--- planet-facts/src/components/Nav.js
+++ planet-facts/src/components/Nav.js
@@ -4,6 +4,7 @@ import { Link } from "react-router-dom";
 import { iconHamburger } from "../assets/addImages";
 function Nav() {
   const [dropDown, SetDropDown] = useState(false);
+
   return (
     <div className="flex justify-between tablet:flex-col desktop:flex-row text-center items-center tablet:items-stretch desktop:items-center bg-mysteriousDepth px-6 tablet:px-[3.1875rem] py-4 tablet:pb-[1.6875rem] tablet:pt-[2rem] desktop:py-[2rem]">
       <p className="uppercase text-[#fff] font-antonio text-[1.75rem] tracking-[-0.0656rem] tablet:pb-[2.4375rem] desktop:pb-0">
@@ -12,8 +13,10 @@ function Nav() {
       <img
         src={iconHamburger}
         className="bg-white tablet:hidden"
+        onClick={() => SetDropDown(!dropDown)}
         alt="geologyEarth"
       />
+
       <ul className="hidden uppercase tablet:flex tablet:justify-between gap-[2.0625rem] opacity-80 text-[#fff] font-leagueSpartan text-[0.6875rem] font-bold leading-[1.5625rem] tracking-[0.0625rem] ">
         <Link to="/mercury">
           <li>Mercury</li>
