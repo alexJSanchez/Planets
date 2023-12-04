acbba77784066300694791dc256a7df42c16df22
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-12-04 02:03:24 -0500

    build nav

--- planet-facts/src/components/Nav.js
+++ planet-facts/src/components/Nav.js
@@ -2,8 +2,10 @@ import "../App.css";
 import { iconHamburger } from "../assets/addImages";
 function Nav() {
   return (
-    <div className="px-6 py-4">
-      <p className="uppercase">The Plantes </p>
+    <div className="flex justify-between items-center bg-mysteriousDepth px-6 py-4">
+      <p className="uppercase text-[#fff] font-antonio text-[1.75rem] tracking-[-0.0656rem]">
+        The Plantes
+      </p>
       <img src={iconHamburger} className="bg-white" alt="geologyEarth" />
     </div>
   );
