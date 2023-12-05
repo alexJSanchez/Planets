f2cb49725f20118e4d714d58ba0a6cd92e111fcb
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-12-05 03:31:06 -0500

    adding styling

--- planet-facts/src/components/buttonOne.js
+++ planet-facts/src/components/buttonOne.js
@@ -1,10 +1,8 @@
 function ButtonOne(props) {
   return (
-    <button className="hidden tablet:flex items-center border pt-buttonOneTop pb-buttonOneBottom text-[#fff] text-[0.75rem] font-bold font-leagueSpartan leading-[1.5625rem] tracking-[0.1607rem] uppercase">
-      <p className="opacity-50 ml-buttonOneLeft mr-[1.75rem] ">
-        {props.number}
-      </p>
-      <p className="opacity-100 mr-buttonTwoRight whitespace-nowrap">
+    <button className="hidden tablet:flex items-center border border-opacity-20 pt-buttonOneTop pb-buttonOneBottom text-[#fff] text-[0.75rem] font-bold font-leagueSpartan leading-[1.5625rem] tracking-[0.1607rem] uppercase">
+      <p className="ml-buttonOneLeft mr-[1.75rem]">{props.number}</p>
+      <p className="mr-buttonTwoRight whitespace-nowrap opacity-100">
         {props.text}
       </p>
     </button>
