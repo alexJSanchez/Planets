37c55bd7d101e222a098e4a4c725ad2c16166ff3
alexjsanchez1020 <alexjsanchez1020@gmail.com>
2023-12-05 01:14:58 -0500

    mercury stable

--- planet-facts/src/components/PlanetDisplay.js
+++ planet-facts/src/components/PlanetDisplay.js
@@ -1,8 +1,24 @@
 function PlanetDisplay(props) {
   return (
-    <div>
-      <div className="flex justify-center items-center py-[5.9375rem] px-[8.25rem]">
+    <div className="text-[#fff]">
+      <div className="flex justify-center items-center pt-[5.9375rem] pb-[6.125rem]  tablet:px-[292px] px-[8.25rem]">
         <img src={props.planetImg} />
+      </div>
+      <div className="flex flex-col items-center text-center px-[1.5rem] ">
+        <h2 className="text-[2.5rem] uppercase font-antonio mb-4">
+          {props.planet}
+        </h2>
+        <p className="text-[0.6875rem] leading-[1.375rem] font-leagueSpartan mb-8">
+          {props.planetSummary}
+        </p>
+        <a href={props.planetSource} className="flex">
+          <p className="text-[0.75rem] leading-[1.5625rem] opacity-50">
+            Source
+          </p>
+          <p className="opacity-50 font-leagueSpartan text-[0.75rem] font-bold leading-[1.5625rem] underline">
+            Wikipedia
+          </p>
+        </a>
       </div>
     </div>
   );
--- planet-facts/src/pages/Mercury.js
+++ planet-facts/src/pages/Mercury.js
@@ -1,14 +1,18 @@
 import React from "react";
 import MobileOverview from "../components/mobileOverview";
 import { planetMercury, planetMercuryInternal } from "../assets/addImages";
+import data from "../data.js";
 import PlanetDisplay from "../components/PlanetDisplay";
 function Mercury() {
   return (
     <div>
       <MobileOverview />
       <PlanetDisplay
+        planet={data.mercury.name}
         planetImg={planetMercury}
+        planetSource={data.mercury.overview.source}
         planetInternalImg={planetMercuryInternal}
+        planetSummary={data.mercury.overview.content}
       />
     </div>
   );
